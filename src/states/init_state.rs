2cb27a96f59477e2e29e16c73b9675d68f5de096
jillpls <jillplspls@gmail.com>
2021-03-01 23:54:27 +0100

    start loading animations properly

--- src/states/init_state.rs
+++ src/states/init_state.rs
@@ -3,9 +3,11 @@
  *   All rights reserved.
  */
 
-use crate::components::animation::AnimationId;
+use crate::components::animation::{AnimationId, AnimationData};
 use crate::components::collision::CollisionBox;
 use crate::entities::{camera::init_camera, PlayerState};
+use crate::resources::loading::load_animations;
+use std::collections::HashMap;
 
 use amethyst::assets::Handle;
 use amethyst::assets::ProgressCounter;
@@ -22,18 +24,36 @@ pub struct InitState {
 impl SimpleState for InitState {
     fn on_start(&mut self, data: StateData<'_, GameData>) {
         self.progress_counter = Some(ProgressCounter::new());
-        let mut progress_counter = self.progress_counter.take().unwrap_or_default();
+        let progress_counter = self.progress_counter.take().unwrap_or_default();
         let resources = data.resources;
 
-        let sheet = import_sheet!("sprites/example/character_run", resources, progress_counter);
+        // let sheet = import_sheet!("sprites/example/character/character_run_debug", resources, progress_counter);
 
+        let mut progress_counter = load_animations("assets/character/animations.ron", resources, progress_counter);
+
+
+        let run_sheet = import_sheet!("character/character_run", resources, progress_counter);
+        let dash_sheet = import_sheet!("character/character_dash", resources, progress_counter);
+        
         let anim = load_animation!(resources, progress_counter, 4);
 
         let mut animation_set = amethyst::animation::AnimationSet::<
             AnimationId,
             amethyst::renderer::SpriteRender,
         >::new();
-        animation_set.insert(AnimationId::Idle, anim);
+        animation_set.insert(AnimationId::Run, anim.clone());
+        animation_set.insert(AnimationId::Dash, anim);
+
+        let mut sheet_map : HashMap<AnimationId, Handle<SpriteSheet>> = HashMap::new();
+        sheet_map.insert(AnimationId::Run, run_sheet.clone());
+        sheet_map.insert(AnimationId::Dash, dash_sheet.clone());
+
+        let anim_data = AnimationData {
+            current_animation : AnimationId::Run,
+            sheet_map,
+            current_time : None
+        };
+
 
         self.progress_counter = Some(progress_counter);
 
@@ -44,9 +64,10 @@ impl SimpleState for InitState {
         let collision_box_debug = collision_box.generate_debug_lines(&transform);
         world.push((
             animation_set,
-            SpriteRender::new(sheet, 0),
+            SpriteRender::new(run_sheet, 0),
+            // SpriteRender::new(run_sheet, 0),
             transform,
-            PlayerState::default(),
+            anim_data,
             collision_box,
             collision_box_debug
         ));
